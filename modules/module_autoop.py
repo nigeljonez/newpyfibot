e71a20ba7050c743ab722fd212272e33917fc1ca
Nigel Jones <nigel@nigelj.com>
2012-09-12 21:59:13 +1200

    Implement RFE, better autoops

--- modules/module_autoop.py
+++ modules/module_autoop.py
@@ -1,5 +1,26 @@
+import sqlite3
+
 def handle_userJoined(bot, user, channel):
     if isAdmin(user):
         nick = getNick(user)
         bot.log("auto-opping %s" % user)
         bot.mode(channel, True, 'o', user=nick)
+    else:
+        conn = sqlite3.connect('karma.db')
+        c = conn.cursor()
+        t = (user,)
+        # Look for user in DB
+        c.execute('select * from autoop where ? glob user', t)
+        res = c.fetchone()
+
+        if res == None:
+            return
+
+        type = res[2]
+        nick = getNick(user)
+
+        if type == 0:
+            bot.mode(channel, True, 'o', user=nick)
+        elif type == 1:
+            bot.mode(channel, True, 'v', user=nick)
+
